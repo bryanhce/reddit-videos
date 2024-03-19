eb2dd516efc2f019b13c7decade8037262cdb493
Ritika Joshi <ritikajoshi0114@gmail.com>
2024-03-19 21:46:20 +0800

    Fix audio bug and test code without else block

--- src/main.py
+++ src/main.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     stroke_color = 'black'
     stroke_width = 1.3
 
-    content = get_reddit_posts(subReddit, nPosts)
-    generate_speech(content) # TODO edit if changing structure
-    create_word_level_JSON()
+    # content = get_reddit_posts(subReddit, nPosts)
+    # generate_speech(content) # TODO edit if changing structure
+    # create_word_level_JSON()
     create_video_with_subtitles(
         base_url,
         font,
