8a76ea9b20352f58c2577ad1e003b95422f92077
bryanhce <ho.cheng.en.bryan@gmail.com>
2024-05-05 22:50:14 +0800

    simple text censorship

--- src/rubislices.py
+++ src/rubislices.py
@@ -4,15 +4,19 @@ from tts import generate_audio_files
 from utils import clean_up
 from abc import ABC, abstractmethod
 from image_edits import generate_image
-from summary import summarise
+from text_transformation import transform_text
 
 # code follows (pseudo) factory design pattern
 
+
 class RubiSlicesBase(ABC):
-    '''
+    """
     Abstract parent class
-    '''
-    def __init__(self, n=1, base_url='', font_url='', color='', stroke_color='', stroke_width=0):
+    """
+
+    def __init__(
+        self, n=1, base_url="", font_url="", color="", stroke_color="", stroke_width=0
+    ):
         self.n = n
         self.base_url = base_url
         self.font_url = font_url
@@ -26,22 +30,26 @@ class RubiSlicesBase(ABC):
 
     def sub_run(self, body):
         generate_audio_files(body)
-        generate_image(body['thumbnail'], self.font_url)
+        generate_image(body["thumbnail"], self.font_url)
         create_word_level_JSON(body)
         create_video_with_subtitles(
             self.base_url,
             self.font_url,
             self.color,
             self.stroke_color,
-            self.stroke_width
+            self.stroke_width,
         )
         clean_up()
 
+
 class Posts(RubiSlicesBase):
-    '''
+    """
     Used to create video from multiple posts in the same subreddit.
-    '''
-    def __init__(self, subreddit, n, base_url, font_url, color, stroke_color, stroke_width):
+    """
+
+    def __init__(
+        self, subreddit, n, base_url, font_url, color, stroke_color, stroke_width
+    ):
         super().__init__(n, base_url, font_url, color, stroke_color, stroke_width)
         self.subreddit = subreddit
 
@@ -54,11 +62,15 @@ class Posts(RubiSlicesBase):
         body = get_reddit_posts(self.subreddit, self.n)
         self.sub_run(body)
 
+
 class Comments(RubiSlicesBase):
-    '''
+    """
     Used to create video from multiple comments in the same post.
-    '''
-    def __init__(self, post_url, n, base_url, font_url, color, stroke_color, stroke_width):
+    """
+
+    def __init__(
+        self, post_url, n, base_url, font_url, color, stroke_color, stroke_width
+    ):
         super().__init__(n, base_url, font_url, color, stroke_color, stroke_width)
         self.post_url = post_url
 
@@ -69,13 +81,14 @@ class Comments(RubiSlicesBase):
         #     'content' : ['test content']
         # }
         body = get_reddit_comments(self.post_url, self.n)
-        summarise(body) # runs depending on the length of text
         self.sub_run(body)
 
+
 class Custom(RubiSlicesBase):
-    '''
+    """
     Used when thumbnail and content are curated by user.
-    '''
+    """
+
     def __init__(self, body, n, base_url, font_url, color, stroke_color, stroke_width):
         super().__init__(n, base_url, font_url, color, stroke_color, stroke_width)
         self.body = body
@@ -84,15 +97,16 @@ class Custom(RubiSlicesBase):
         print("Processing custom content")
         # place content in array so don't have to create
         # additional steps to process the content
-        self.body['content'] = [self.body['content']]
-        summarise(self.body) # runs depending on the length of text
+        self.body["content"] = [self.body["content"]]
+        transform_text(self.body)
         self.sub_run(self.body)
+
 
 class RubiSlicesFactory:
     @staticmethod
     def create(subreddit=None, post_url=None, body=None, **kwargs):
         if body:
-            return Custom(body=body, **kwargs) 
+            return Custom(body=body, **kwargs)
         elif subreddit and post_url:
             raise ValueError("Either subreddit or post_url is provided not both")
         elif subreddit:
